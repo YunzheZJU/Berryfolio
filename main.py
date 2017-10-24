ce5e5c4424a451f8727225a4c5e8c17734612032
Yunzhe <iLoveLive@honoka.moe>
2017-10-25 00:05:05 +0800

    结合Flask_wtf实现文件上传

--- main.py
+++ main.py
@@ -3,12 +3,16 @@
 import os
 from utils import make_dirs
 import dbOperation
-from flask import Flask, request, send_from_directory
+from flask import Flask, render_template
 from flask_uploads import UploadSet, configure_uploads, IMAGES, patch_request_class
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+from wtforms import SubmitField
 
 
 # 我也不知道为什么，总之这句必须有
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Thisismykeyafuibsvseibgf'
 app.config['UPLOADED_PHOTOS_DEST'] = os.path.join(os.path.abspath("."), "data")  # 文件储存地址
 make_dirs([app.config['UPLOADED_PHOTOS_DEST']])
 
@@ -17,24 +21,22 @@ configure_uploads(app, photos)
 patch_request_class(app)  # 文件大小限制，默认为16MB
 
 
-html = '''
-    <!DOCTYPE html>
-    <title>Upload File</title>
-    <h1>图片上传</h1>
-    <form method=post enctype=multipart/form-data>
-         <input type=file name=photo>
-         <input type=submit value=上传>
-    </form>
-    '''
+class UploadForm(FlaskForm):
+    photo = FileField(validators=[
+        FileAllowed(photos, u'只能上传图片！'),
+        FileRequired(u'文件未选择！')])
+    submit = SubmitField(u'上传')
 
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
-    if request.method == 'POST' and 'photo' in request.files:
-        filename = photos.save(request.files['photo'])
+    form = UploadForm()
+    if form.validate_on_submit():
+        filename = photos.save(form.photo.data)
         file_url = photos.url(filename)
-        return html + '<br><img src=' + file_url + '>'
-    return html
+    else:
+        file_url = None
+    return render_template('test.html', form=form, file_url=file_url)
 
 
 # # 这是通往网站首页的大门
