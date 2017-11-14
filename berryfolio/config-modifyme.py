fe49ebb64f8e304ded538b1002f26e0816f50092
Yunzhe <iLoveLive@honoka.moe>
2017-11-14 10:16:08 +0800

    更新config.py

---
+++ berryfolio/config-modifyme.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+GLOBAL = {
+    'DB_PATH': "d:\\projects\\pycharm\\dams\\berryfolio.db",
+    'LOG_PATH': "d:\\projects\\pycharm\\dams\\log.log"
+}
--- berryfolio/config.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-GLOBAL = {
-    'DB_PATH': "e:\\projects\\pycharm\\dams\\berryfolio.db",
-    'LOG_PATH': "e:\\projects\\pycharm\\dams\\log.log"
-}
